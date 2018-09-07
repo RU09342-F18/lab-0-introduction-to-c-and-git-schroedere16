e760e9c98c80e7e14528c318078aa6dd4158a243
schroedere16 <36168890+schroedere16@users.noreply.github.com>
2018-09-07 16:33:14 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,1 +1,7 @@
+#ifndef MATH_H_
+#define MATH_H_
 
+//Part of your documentation should be listing the valid inputs and outputs for the functions you create.
+int math(int num1, int num2, char Operator);
+
+#endif /* MATH_H_ */
