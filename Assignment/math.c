85f8e7fc4738c09e1720d819358ced41f0c943ae
schroedere16 <36168890+schroedere16@users.noreply.github.com>
2018-09-09 17:33:44 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -8,7 +8,7 @@ Author: Eric Schroeder*/
 
 int main(int num1, int num2, char Operator)
 {
-   
+   //User can define specific values for num1 and num2 here if desired
     Operator = num1+num2; //inputs must be real numbers 
     printf("num1+num2 = %d \n",Operator);
     Operator = num1-num2; //inputs must be real numbers
