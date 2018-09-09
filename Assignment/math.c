d752b79880e109ae337445ece7da687c141c7306
schroedere16 <36168890+schroedere16@users.noreply.github.com>
2018-09-09 15:07:50 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,7 +1,37 @@
-#ifndef MATH_H_
-#define MATH_H_
+/*
+created September 9, 2018
+Last updated September 9, 2018
+Author: Eric Schroeder*/
 
-//Part of your documentation should be listing the valid inputs and outputs for the functions you create.
-int math(int num1, int num2, char Operator);
+#include <stdio.h>
+#include <math.h>
 
-#endif /* MATH_H_ */
+int main(int num1, int num2, char Operator)
+{
+   
+    Operator = num1+num2; //inputs must be real numbers 
+    printf("num1+num2 = %d \n",Operator);
+    Operator = num1-num2; //inputs must be real numbers
+    printf("num1-num2 = %d \n",Operator);
+    Operator = num1*num2; //inputs must be real numbers
+    printf("num1*num2 = %d \n",Operator);
+    Operator=num1/num2; //inputs must be real numbers, decimal answers are displayed as zero
+    printf("num1/num2 = %d \n",Operator);
+    Operator=num1%num2; //inputs must be real numbers
+    printf("Remainder when a divided by num2 = %d \n",Operator);
+    Operator = num1<<num2; //inputs must be real numbers
+    printf ("num1 shifted left by num2 = %d \n", Operator);
+    Operator = num1>>num2; //inputs must be real numbers
+    printf ("num1 shifted right by num2 = %d \n", Operator);
+    Operator = num1&num2; //inputs must be real numbers
+    printf ("Bitwise AND of num1 and num2 = %d \n", Operator);
+    Operator = num1|num2; //inputs must be real numbers
+    printf ("Bitwise OR of num1 sand num2 = %d \n", Operator);
+    Operator = num1^num2; //inputs must be real numbers
+    printf ("Bitwise XOR of num1 and num2 = %d \n", Operator);
+    Operator = ~num1; //inputs must be real number
+    printf ("Bitwise inverse of num1 = %d \n", Operator);
+    Operator = ~num2; //input must be a real number
+    printf ("Bitwise inverse of num2 = %d \n", Operator);
+    return 0;
+}
